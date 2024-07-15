621bd23b97b13d995e13187d865aaaffb6484a9a
Jimmy Woolley <jamesmwoolley@gmail.com>
2024-07-14 23:00:40 -0600

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import {Food, FoodAmount, Meal, Diet, DietPlan } from './types'
 
-function App() {
+const App: React.FC = () => {
+  const [diet, setDiet] = useState<Diet | null>(null);
+  const [foods, setFoods] = useState<{ [key: string]: Food }>({});
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/diet-plan.json');
+      if (!response.ok) { throw new Error(response.statusText); }
+      const dietPlan: DietPlan = await response.json();
+
+      const foods: { [key: string]: Food } = {};
+      dietPlan.foods.forEach(food => {
+        foods[food.key] = food;
+      });
+      setFoods(foods);
+
+      const diet = dietPlan.diets.find(diet => diet.key === 'jimmy-chow');
+      if (!diet) { throw new Error('jimmy-chow not found'); }
+      setDiet(diet);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <p>loading...</p>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{diet?.name}</h2>
+      <h3>Meals</h3>
+      <ul>
+        {diet?.meals.map((meal: Meal, index: number) => (
+          <li key={index}>
+            <h4>{meal.name}</h4>
+            <ul>
+              {meal.foodAmounts.map((foodAmount: FoodAmount, foodAmountIndex: number) => {
+                const food = foods[foodAmount.key];
+                return (
+                  <li key={foodAmountIndex}>
+                    {`${foodAmount.amount} ${food?.unit == '' ? food?.unit : food?.unit + ' of '} ${food?.name}`}
+                  </li>
+                );
+              })}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/types.tsx
@@ -0,0 +1,29 @@
+export type Food = {
+	key: string,
+	name: string,
+	unit: string,
+	protein: number,
+	carbs: number,
+	fat: number
+};
+
+export type FoodAmount = {
+	key: string,
+	amount: number
+};
+
+export type Meal = {
+	name: string,
+	foodAmounts: FoodAmount[]
+};
+
+export type Diet = {
+	name: string,
+	key: string,
+	meals: Meal[]
+}
+
+export type DietPlan = {
+	foods: Food[],
+	diets: Diet[]
+};
