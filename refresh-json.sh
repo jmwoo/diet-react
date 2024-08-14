2cf434616433199ca088153347fc9337209c8d5f
Jimmy Woolley <jamesmwoolley@gmail.com>
2024-08-14 13:20:28 -0600

    update json

--- refresh-json.sh
+++ refresh-json.sh
@@ -1,1 +1,1 @@
-wget -O ./public/assets/diet-plan.json https://gist.githubusercontent.com/jmwoo/13a5fdcc8e07dbf81267289ac011412e/raw/8cbf1f6773c5f2e219ffd8045f4740c48ea5eda3/diet-plan.json
+wget -O ./public/assets/diet-plan.json https://raw.githubusercontent.com/jmwoo/static-data/main/diet-plan.json
